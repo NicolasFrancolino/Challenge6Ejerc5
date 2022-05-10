73b29c700a7e0cb3c6e757c9481122b55f031073
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-05-10 13:46:28 -0300

    Agregar archivos de proyecto.

---
+++ Challenge6Ejerc5/Pilas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejerc5
+{
+    class Pilas<T>
+    {
+        public Stack<T> pila = new Stack<T>();
+
+        public void Apilar(T objeto)
+        {
+            pila.Push(objeto);
+        }
+
+        public T Desapilar()
+        {
+            return pila.Pop();
+        }
+
+
+        public void MostrarPila()
+        {
+            foreach (T objeto in pila)
+            {
+                Console.WriteLine(objeto);
+            }
+            
+        }
+    }
+}
---
+++ Challenge6Ejerc5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejerc5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilas<String> pilas = new Pilas<String>();
+            pilas.Apilar("Hola");
+            pilas.Apilar("Soy");
+            pilas.Apilar("Nicolas");
+            pilas.MostrarPila();
+
+            Pilas<double> pilas1 = new Pilas<double>();
+            pilas1.Apilar(3);
+            pilas1.Apilar(4.6);
+            pilas1.Apilar(21);
+            pilas1.MostrarPila();
+            Console.WriteLine();
+            pilas1.Desapilar();
+            pilas1.MostrarPila();
+            Console.ReadKey();
+        }
+    }
+}
